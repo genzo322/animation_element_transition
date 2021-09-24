88960b2c771607c6e36aaab63c523d5f209aea56
Phung Thanh Toan <toanpt322@gmail.com>
2021-09-24 14:27:45 +0700

    Animation_element_transition

---
+++ app/src/main/java/com/example/animation_transition/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.animation_transition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                overridePendingTransition(R.anim.enter_x, R.anim.exit_x);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/animation_transition/MainActivity4.java
@@ -0,0 +1,26 @@
+package com.example.animation_transition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+            }
+        });
+
+    }
+}
